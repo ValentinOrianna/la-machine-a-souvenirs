59416710ada028bc23964463988b7e039bda3b68
ValentinOrianna <steup33@gmail.com>
2026-06-09 15:58:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-script.js
+document.getElementById("start").addEventListener("click", () => {
+
+    document.body.style.transition = "1s";
+
+    document.body.style.background = "#2b1b08";
+
+    setTimeout(() => {
+
+        alert("⚙️ La Machine à Souvenirs est en marche...");
+
+    }, 1000);
+
+});
