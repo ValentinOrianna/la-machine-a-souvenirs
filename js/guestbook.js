da44e77d89e23e555e8337049a1ffd87f286de6c
ValentinOrianna <steup33@gmail.com>
2026-06-14 10:22:35 +0000

    Ajout date livre or

--- js/guestbook.js
+++ js/guestbook.js
@@ -3,6 +3,7 @@ async function chargerLivreOr() {
     const container = document.getElementById("guestbookMessages");
 
     container.innerHTML = "";
+    console.log(entree);
 
     const { data, error } = await supabaseClient
         .from("guestbook")
@@ -18,11 +19,26 @@ async function chargerLivreOr() {
 
         const bloc = document.createElement("div");
 
-        bloc.innerHTML = `
-            <strong>${entree.prenom}</strong><br>
-            ${entree.message}
-            <hr>
-        `;
+       const date = new Date(entree.created_at);
+
+bloc.innerHTML = `
+    <strong>${entree.prenom}</strong><br>
+
+    <small>
+        ${date.toLocaleDateString("fr-FR")}
+        à
+        ${date.toLocaleTimeString("fr-FR", {
+            hour: "2-digit",
+            minute: "2-digit"
+        })}
+    </small>
+
+    <br><br>
+
+    ${entree.message}
+
+    <hr>
+`;
 
         container.appendChild(bloc);
 
