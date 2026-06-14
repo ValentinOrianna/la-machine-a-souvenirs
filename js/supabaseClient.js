45b818aa5ef63ffae452dd07c26a08494c7bcecf
ValentinOrianna <steup33@gmail.com>
2026-06-14 16:13:36 +0000

    fix supabase api key

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://ganeyvtjqzdptydltrbs.supabase.co";
-const SUPABASE_KEY = "sb_publishable_...";
+
+const SUPABASE_KEY = "sb_publishable_-xzhTlz6UWYMpsCNtaXe_w_372b5Ai0";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
